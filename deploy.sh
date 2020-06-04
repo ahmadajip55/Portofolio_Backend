34af66a537e6995dee702e97856135c6af7de8ec
ahmadajip55 <pangestu@alterra.id>
2020-06-04 06:02:51 -0400

    first cicd

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+eval "$(ssh-agent -s)" &&
+ssh-add -k ~/.ssh/id_rsa &&
+#cd /ajay-task/www/Portofolio_Backend #helloworld
+#git pull
+
+source ~/.profile
+echo "$DOCKER_PASSWORD" | docker login --username $DOCKER_USERNAME --password-stdin
+docker stop backend
+docker rm backend
+docker rmi ahmadajip55/rest-tutorial:latest
+docker run -d --name restdemo -p 5000:5000 ahmadajip55/rest-tutorial:latest
